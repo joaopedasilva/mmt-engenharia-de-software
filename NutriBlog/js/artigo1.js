d64e9f9273a8cffc6ebfc03d767f6b32cdff24f9
Déivson Bruno <117856490+deivsonbru18@users.noreply.github.com>
2024-05-01 19:49:50 -0300

    Entrega da segunda Sprint
    Entrega da segunda Sprint

---
+++ NutriBlog/js/artigo1.js
@@ -0,0 +1,24 @@
+// Função para abrir o popup de edição
+function openEditPopup() {
+    document.getElementById("editPopup").style.display = "block";
+}
+
+// Função para fechar o popup de edição
+function closeEditPopup() {
+    document.getElementById("editPopup").style.display = "none";
+}
+
+// Função para abrir o popup de exclusão
+function openDeletePopup() {
+    document.getElementById("deletePopup").style.display = "block";
+}
+
+// Função para fechar o popup de exclusão
+function closeDeletePopup() {
+    document.getElementById("deletePopup").style.display = "none";
+}
+
+// Função para excluir o artigo
+function deleteArticle() {
+    // Adicione o código para excluir o artigo aqui
+}
